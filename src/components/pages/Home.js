cbc1fcc8f864e40ba363ab163b869b55ff55d0ea
Somer Jackson <somer.jackson@gmail.com>
2021-02-07 19:48:27 -0500

    add home page

---
+++ src/components/pages/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import profilePic from '../../../src/assets/somer_jackson.jpeg';
+
+export default function Home() {
+    return (
+        <div className="container p-5">
+            <div className="card text-left p-5">
+                <h1 className="header">About Me</h1>
+                <div className="d-flex">
+                    <img className="pr-5" src={profilePic} alt="Somer Jackson Profile Picture"/>
+                    <p className="text-left">I am Somer Jackson , a former customer service representative who is currently enrolled in the Full Stack coding program at Georgia Tech. I am now more than halfway into the cohort and the work has gotten more indepth .The newest project showcased the more intricate aspects of coding. Have a look at some of the projects I have worked on.</p>
+                </div> 
+            </div>
+        </div>
+        
+    )
+}
