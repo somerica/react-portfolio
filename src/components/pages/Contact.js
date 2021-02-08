e8108dc25a78a2490522356dd6fa37164d5b3d69
Somer Jackson <somer.jackson@gmail.com>
2021-02-07 19:47:38 -0500

    add contact page

---
+++ src/components/pages/Contact.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function Contact() {
+    return (
+        <div className="container">
+            <div className="card p-5">
+                <h1 className="header">Contact Information</h1>
+                <ul style={{listStyle: 'none'}}>
+                    <li>Phone: (404) 344-1808</li>
+                    <li>Email: somer.jackson@gmail.com</li>
+                    <li><a href="https://www.linkedin.com/in/somer-jackson-0a5136b6/">LinkedIn Profile</a></li>
+                    <li><a href="https://github.com/somerica">Github Profile</a></li>
+                </ul>
+            </div>
+        </div>
+        
+    )
+}
